42ab426de8be292fb24b0c0b29b8b335df501917
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-09 11:39:46 +0700

    jlkgj

---
+++ IdeaProjects/AccessModifier_staticProperty_staticMethod/src/mypack/B.java
@@ -0,0 +1,17 @@
+//save by B.java
+
+package mypack;
+
+import pack.*;
+
+class B{
+
+    public static void main(String args[]){
+
+        A obj = new A();//Compile Time Error
+
+        obj.msg();//Compile Time Error
+
+    }
+
+}
